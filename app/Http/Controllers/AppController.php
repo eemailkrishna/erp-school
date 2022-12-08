a212ae6e4a532e40afd6435f70b5618f26983020
krishna9140 <eemail.krishna@gmail.com>
2022-12-08 14:54:25 +0530

    first

---
+++ app/Http/Controllers/AppController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AppController extends Controller
+{
+    public function android_app(){
+        return view('android_app.android_app');
+        //  this is testin purpose
+    }
+}
