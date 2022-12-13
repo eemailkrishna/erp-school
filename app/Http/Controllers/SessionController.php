34c9787d0fbae4c20a2c972d4e9f3d099cfa31d5
dinkarchauhan <dinkarchauhan720@gmail.com>
2022-12-13 16:58:47 +0530

    first commit

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -9,4 +9,12 @@ class SessionController extends Controller
     public function session(){
         return view('session.session');
     }
+
+    public function AddSession(){
+        return view('session.add_session');
+    }
+
+    public function MoveStudent(){
+        return view('session.move_student');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -64,7 +64,19 @@ Route::get('/staff',[StaffController::class,'staff'])->name('staff');
 Route::get('/student',[StudentController::class,'student'])->name('student');
 
 // Account Routes
-Route::get('/account',[AccountController::class,'account'])->name('account');
+Route::get('/account',[AccountController::class,'Account'])->name('account');
+Route::get('/add-account',[AccountController::class,'AddAccount'])->name('add-account');
+Route::get('/account-list',[AccountController::class,'AccountList'])->name('account-list');
+Route::get('/add-income-or-expence-info',[AccountController::class,'AddIncomeOrExpenceInfo'])->name('add-income-or-expence-info');
+Route::get('/income-or-expence-list',[AccountController::class,'IncomeOrExpenceList'])->name('income-or-expence-list');
+Route::get('/ledger',[AccountController::class,'Ledger'])->name('ledger');
+Route::get('/ledger-report',[AccountController::class,'LedgerReport'])->name('ledger-report');
+Route::get('/ledger-report-monthly',[AccountController::class,'LedgerReportMonthly'])->name('ledger-report-monthly');
+Route::get('/income-expense-report',[AccountController::class,'IncomeExpenseReport'])->name('income-expense-report');
+Route::get('/refund-report',[AccountController::class,'RefundReport'])->name('refund-report');
+Route::get('/ledger-advance-salary-report',[AccountController::class,'LedgerAdvanceSalaryReport'])->name('ledger-advance-salary-report');
+
+
 
 // Dues Routes
 Route::get('/dues',[DuesController::class,'dues'])->name('account');
@@ -84,11 +96,26 @@ Route::get('/certificate',[CertificateController::class,'certificate'])->name('c
 Route::get('/examination',[ExaminationController::class,'examination'])->name('examination');
 
 // homework
-Route::get('/homework',[HomeWorkController::class,'homework'])->name('homework');
+Route::get('/homework',[HomeWorkController::class,'Homework'])->name('homework');
+Route::get('/homework-add',[HomeWorkController::class,'HomeworkAdd'])->name('homework-add');
+Route::get('/homework-list',[HomeWorkController::class,'HomeworkList'])->name('homework-list');
+Route::get('/add-classwork',[HomeWorkController::class,'AddClasswork'])->name('add-classwork');
+Route::get('/classwork-list',[HomeWorkController::class,'ClassworkList'])->name('classwork-list');
+
+
+
+
+
 
 // set paper routes
 
 Route::get('/set-paper',[SetPaperController::class,'SetPaper'])->name('set-paper');
+Route::get('/add-question',[SetPaperController::class,'AddQuestion'])->name('add-question');
+Route::get('/view-question',[SetPaperController::class,'ViewQuestion'])->name('view-question');
+Route::get('/instant-go-to-paper-setter',[SetPaperController::class,'InstantGoToPaperSetter'])->name('instant-go-to-paper-setter');
+Route::get('/go-to-paper-setter',[SetPaperController::class,'GoToPaperSetter'])->name('go-to-paper-setter');
+Route::get('/total-paper-list',[SetPaperController::class,'TotalPaperList'])->name('total-paper-list');
+
 
 // complaint route
 Route::get('/complaint',[ComplaintController::class,'complaint'])->name('complaint');
@@ -109,18 +136,48 @@ Route::get('/event',[EventController::class,'event'])->name('event');
 // sport
 
 Route::get('/sports',[SportController::class,'sport'])->name('sport');
+Route::get('/add-sports',[SportController::class,'AddSport'])->name('add-sport');
+Route::get('/sports-type',[SportController::class,'SportType'])->name('sport-type');
+Route::get('/age-category',[SportController::class,'AgeCategory'])->name('age-category');
+Route::get('/add-participate',[SportController::class,'AddParticipate'])->name('add-participate');
+Route::get('/participate-list',[SportController::class,'ParticipateList'])->name('participate-list');
+Route::get('/team-creation',[SportController::class,'TeamCreation'])->name('team-creation');
+Route::get('/team-creation-list',[SportController::class,'TeamCreationList'])->name('team-creation-list');
+
+
+
+
 
 //  download routes
 Route::get('/download',[DownloadController::class,'download'])->name('download');
 
 //  recycle bin routes
-Route::get('/recycle-bin',[RecycleBinController::class,'recylebin'])->name('recycle');
+Route::get('/recycle-bin',[RecycleBinController::class,'RecycleBin'])->name('recycle-bin');
+Route::get('/recycle-student-admission-list',[RecycleBinController::class,'RecycleStudentAdmissionList'])->name('recycle-student-admission-list');
+Route::get('/recycle-employee-list',[RecycleBinController::class,'RecycleEmployeeList'])->name('recycle-employee-list');
+Route::get('/recycle-expense-list',[RecycleBinController::class,'RecycleExpenseList'])->name('recycle-expense-bin');
+Route::get('/recycle-hostel-student-list',[RecycleBinController::class,'RecycleHostelStudentList'])->name('recycle-hostel-student-list');
+Route::get('/recycle-hostel-account-list',[RecycleBinController::class,'RecycleHostelAccountList'])->name('recycle-hostel-account-list');
+Route::get('/student-registration-list',[RecycleBinController::class,'StudentRegistrationList'])->name('student-registration-list');
+
+
 
 // session  routes
 Route::get('/session',[SessionController::class,'session'])->name('session');
+Route::get('/add-session',[SessionController::class,'AddSession'])->name('add-session');
+Route::get('/move-student',[SessionController::class,'MoveStudent'])->name('move-student');
+
+
+
 
 // reminder routes
 Route::get('/reminder',[ReminderCotroller::class,'reminder'])->name('reminder');
+Route::get('/reminder-add',[ReminderCotroller::class,'ReminderAdd'])->name('reminder-add');
+Route::get('/reminder-list',[ReminderCotroller::class,'ReminderList'])->name('reminder-list');
+Route::get('/reminder-teacher-add',[ReminderCotroller::class,'ReminderTeacherAdd'])->name('reminder-teacher-add');
+Route::get('/reminder-teacher-list',[ReminderCotroller::class,'ReminderTeacherList'])->name('reminder-teacher-list');
+
+
 
 //  govt routes
 Route::get('/govt-requirement',[GovRequireController::class,'gov_requirement'])->name('gov-requirement');
